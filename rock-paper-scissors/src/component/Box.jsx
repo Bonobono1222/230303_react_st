774212e665133e992b71d6433f8d922a8d02c735
doo <ekseksgody@hanmail.net>
2023-03-21 12:34:43 +0900

    ⚡️ 재커밋

--- rock-paper-scissors/src/component/Box.jsx
+++ rock-paper-scissors/src/component/Box.jsx
@@ -14,15 +14,16 @@ const Box = (props) => {
 
   let result;
   if(props.title === "Computer" && props.result !== "Tie" && props.result !== "" ) {
-    // props.title이 Computer이고 props.result가 비겼어요~가 아니고 props.result가 빈 문자열이 아닐 때
+    // props.title이 Computer이고 props.result가 Tie가 아니고 props.result가 빈 문자열이 아닐 때
       result = props.result === "Win" ? "Lose" : "Win";
-    // props.result가 이겼어요!이면 result2에 졌어요..를 반환하고 아니면 이겼어요!를 반환해라
+    // props.result가 Win면 result에 Lose를 반환하고 아니면 Win을 반환해라
   } else {
       result = props.result;
   }
 
   return (
       <div className={`box ${result}`}>
+      {/* result값이 Win이냐 Lose냐에 따라 그 이름의 클래스가 추가된다. */}
       <h1>{props.title}</h1>
       <h2>{props.item && props.item.name}</h2>
       {/* props.item에 값이 있을 때 props.item.name을 렌더링 해라 */}
